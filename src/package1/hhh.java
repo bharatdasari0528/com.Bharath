e40469075f48b6fa0750a323084f418e7f24d667
DASARI YAGNEWAR BHARAT <yagneshdasari0528@gmail.com>
2025-01-02 11:17:12 +0530

    Update hhh.java
    code is readding

--- src/package1/hhh.java
+++ src/package1/hhh.java
@@ -1,12 +1,15 @@
 package package1;
 
 public class hhh {
-
+int id=10;
+String ="BHARAT"
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println(2165+6548);
   System.out.println(21135+31465);
-		System.out.println("This Is Bharat");
-	}
+ System.out.println("This Is Bharat");
+	System.out.println("Student"+id);
+	System.out.println("Name"+String);
+		}
 
 }
