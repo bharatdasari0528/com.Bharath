d550278b68422c31bf19b4643110e378eef1293a
DASARI YAGNEWAR BHARAT <yagneshdasari0528@gmail.com>
2025-01-02 11:27:26 +0530

    hhh
    again code has be changed

--- src/package1/hhh.java
+++ src/package1/hhh.java
@@ -1,15 +1,17 @@
 package package1;
 
+
 public class hhh {
 int id=10;
-String ="BHARAT"
+String name ="BHARAT";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println(2165+6548);
   System.out.println(21135+31465);
  System.out.println("This Is Bharat");
-	System.out.println("Student"+id);
-	System.out.println("Name"+String);
+   hhh t=new hhh();
+   System.out.println("Student"+t.id);
+	System.out.println("Name"+t.name );
 		}
 
 }
