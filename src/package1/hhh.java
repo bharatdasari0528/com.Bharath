4241f94147809fc5cd2578df4b3c01d1c0a48be4
DASARI YAGNEWAR BHARAT <yagneshdasari0528@gmail.com>
2025-01-02 11:07:51 +0530

    hh
    new code

---
+++ src/package1/hhh.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class hhh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println(2165+6548);
+  System.out.println(21135+31465);
+	}
+
+}
