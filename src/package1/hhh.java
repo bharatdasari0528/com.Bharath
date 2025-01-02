15ab8dfdca929e037a2a1eb7369dc5d46ae05bfc
DASARI YAGNEWAR BHARAT <yagneshdasari0528@gmail.com>
2025-01-02 11:12:12 +0530

    hh
    new code corrcet

--- src/package1/hhh.java
+++ src/package1/hhh.java
@@ -6,7 +6,7 @@ public class hhh {
 		// TODO Auto-generated method stub
   System.out.println(2165+6548);
   System.out.println(21135+31465);
-		system.out.println("This Is Bharat"):
+		System.out.println("This Is Bharat");
 	}
 
 }
